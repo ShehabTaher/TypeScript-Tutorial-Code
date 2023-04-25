09547c29438bb20710f307a98d0d8a9cac28cfa1
Shehab Taher <shehab.taher93@gmail.com>
2023-04-25 21:49:20 +0200

    first commit

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+let theName = "Shehab";
+let theAge = 29;
+let hire = false;
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,21 @@
+/*
+ Video [3]
+ */
+// console.log(Math.floor(10.14))
+
+/*
+ Video [4] Statically vs Dynamically Typed Languages
+ */
+// let age = 20
+// if (age > 30 ){
+//     console.log("Good")
+// }else {
+//     console.log(age.repeat(3))
+// }
+
+/*
+ Video [5] Type Annotations || Signature
+ */
+let theName: string = "Shehab"
+let theAge: number = 29
+let hire: boolean = false
